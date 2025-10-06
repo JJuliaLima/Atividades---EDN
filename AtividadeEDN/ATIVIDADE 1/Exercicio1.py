4384aa61b4ab7e6cf2ab72eafedf3cf2b9f38f4a
Julia Lima <161635220+JJuliaLima@users.noreply.github.com>
2025-10-05 23:39:55 -0300

    Criando repositório, adicionando arquivos

---
+++ AtividadeEDN/ATIVIDADE 1/Exercicio1.py
@@ -0,0 +1,4 @@
+#  Programa de Saudação
+# * Crie um programa que imprima a mensagem "Olá, mundo!" na tela.
+
+print('olá mundo, boa tarde Ricardo')
---
+++ AtividadeEDN/ATIVIDADE 1/Exercício4.py
@@ -0,0 +1,14 @@
+# 4- Calculadora de Preço Total
+# * Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+# * Nome do produto: "Cadeira Infantil"
+# * Preço unitário: R$ 12.40
+# * Quantidade: 3
+# O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+
+produto = 'Cadeira Infantil'
+quantidade = 3
+preco = 12.40
+total = preco * quantidade
+
+print(f'Na compra de {quantidade} unidades da {produto}, o valor final é de {total}. Qual é a forma de pagamento?')
